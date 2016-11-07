073b52cce52085b333d949ddd864f03af7dccd18
Jessica Mathis <jbmathis@umich.edu>
2016-11-06 21:49:57 -0500

    commented code

--- HW3/bshw3.py
+++ HW3/bshw3.py
@@ -10,18 +10,19 @@
 
 # Deliverables
 # Make sure the new page is uploaded to your GitHub account.
+
 import requests
 from bs4 import BeautifulSoup
-
+#use beautifulsoup to connect to the umsi website
 base_url = 'http://collemc.people.si.umich.edu/data/bshw3StarterFile.html'
 r = requests.get(base_url)
 soup = BeautifulSoup(r.text, "html.parser")
-
+#convert the html file for the website to one large string
 html_str = soup.prettify(formatter=None)
-
+#replace student with AMAZING student, change main photo, and add UMSI logos
 html_str = html_str.replace('student', 'AMAZING student')
 html_str = html_str.replace('https://testbed.files.wordpress.com/2012/09/bsi_exposition_041316_192.jpg', 'media/IMG_6197.JPG')
 html_str = html_str.replace('logo2.png', 'media/logo.png')
-
+#open index.html and write the changed string to the file
 fout = open('index.html','w')
 fout.write(html_str)
--- HW3/madlibhw3.py
+++ HW3/madlibhw3.py
@@ -14,24 +14,28 @@ import random
 from nltk.book import text2
 
 print("START*******\n\n")
+#since text is already tokenized, deal with only the first 150 elements of the list and tag them
 text2 = text2[0:150]
 tagged_tokens = nltk.pos_tag(text2)
 tagmap = {"NN":"a noun","NNS":"a plural noun","VBD":"a past-tense verb","JJ":"an adjective", "IN":"a preposition"}
 substitution_probabilities = {"NN":.15,"NNS":.1,"VBD":.1,"JJ":.1,"IN":.1}
+#concatenate the tokens to a string to be printed out to the terminal
 string = ""
 for token in text2:
 	string += ' ' + token
 print('Original Text\n--------------')
 print(string)
-
+#iterate through the text and replace the parts of speech at the specified probabilities
 changed_text = []
 final_text = ""
 for (word, tag) in tagged_tokens:
 	if tag not in substitution_probabilities or random.random() > substitution_probabilities[tag]:
 		changed_text.append(word)
 	else:
+		#prompt user to input a word that matches the part of speech of the current word
 		new_word = input("Please enter %s:\n" % (tagmap[tag]))
 		changed_text.append(new_word)
+#concatenate the words in the list to a string to be printed out to terminal
 for token in changed_text:
 	final_text += ' ' + token
 print('\nChanged Text\n---------------')
